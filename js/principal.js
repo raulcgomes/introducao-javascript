3a2c4107a3e531fd54b72ba15eb7dac33f542764
Raul Crisostomo Gomes <raul.cgomes92@gmail.com>
2018-08-09 00:55:32 -0300

    Aula 02

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,33 @@
-console.log('carregado de arquivo externo.');
-
-var titulo = document.querySelector('.titulo');
-
+var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdAltura = paciente.querySelector(".info-altura");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdImc = paciente.querySelector(".info-imc");
+
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+} else {
+	tdImc.textContent = "Altura e/ou peso invalidos";
+}
